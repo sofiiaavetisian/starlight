7c7b710ee4bc3488c21a19bac206339d8f6151d3
Sofiia Avetisian <savetisian.ieu2023@student.ie.edu>
2025-09-10 17:28:14 +0200

    changed settings file

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",     
+    "satellites", 
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [ BASE_DIR / "satellites" / "static" ]
+TEMPLATE_DIR = BASE_DIR / "satellites" / "templates"
+from pathlib import Path
+TEMPLATES[0]["DIRS"] = [TEMPLATE_DIR]
