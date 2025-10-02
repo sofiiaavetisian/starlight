6366e669d26660e0a3dff1496c556a51a8c7a3d7
Sofiia Avetisian <savetisian.ieu2023@student.ie.edu>
2025-10-03 01:03:38 +0200

    fixed issue with the login link

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -124,6 +126,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "static/"
-TEMPLATE_DIR = BASE_DIR / "satellites" / "templates"
-from pathlib import Path
-TEMPLATES[0]["DIRS"] = [TEMPLATE_DIR]
